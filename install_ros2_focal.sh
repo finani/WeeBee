4ccd42c091ffe9f3b33844478e0706524fd7552c
Inhwan Wee <inhwan.wee@nearthlab.com>
2021-10-08 23:50:51 +0900

    Add files via upload

---
+++ install_ros2_focal.sh
@@ -0,0 +1,119 @@
+# Set locale
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+# Setup Sources
+sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install ROS2 packages
+sudo apt update && sudo apt install -y ros-foxy-desktop
+source /opt/ros/foxy/setup.bash
+
+# Install Development packages
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  libbullet-dev \
+  python3-colcon-common-extensions \
+  python3-flake8 \
+  python3-pip \
+  python3-pytest-cov \
+  python3-rosdep \
+  python3-setuptools \
+  python3-vcstool \
+  wget
+
+python3 -m pip install -U \
+  argcomplete \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest
+
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev \
+  libcunit1-dev
+
+# Set up a workspace 
+source /opt/ros/foxy/setup.bash
+mkdir -p ~/robot_ws/src
+cd ~/robot_ws/
+colcon build --symlink-install
+
+# Set Alias
+echo "
+
+# Set ROS
+source /opt/ros/foxy/setup.bash
+source ~/robot_ws/install/local_setup.bash
+
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+source /usr/share/vcstool-completion/vcs.bash
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=~/robot_ws
+
+export ROS_DOMAIN_ID=7
+export ROS_NAMESPACE=robot1
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+# export RMW_IMPLEMENTATION=rmw_connext_cpp
+# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# export RMW_IMPLEMENTATION=rmw_gurumdds_cpp
+
+# export RCUTILS_CONSOLE_OUTPUT_FORMAT='[{severity} {time}] [{name}]: {message} ({function_name}() at {file_name}:{line_number})'
+export RCUTILS_CONSOLE_OUTPUT_FORMAT='[{severity}]: {message}'
+export RCUTILS_COLORIZED_OUTPUT=1
+export RCUTILS_LOGGING_USE_STDOUT=0
+export RCUTILS_LOGGING_BUFFERED_STREAM=1
+
+# Set ROS Alias Command
+alias cw='cd ~/robot_ws'
+alias cs='cd ~/robot_ws/src'
+alias ccd='colcon_cd'
+
+alias cb='cd ~/robot_ws && colcon build --symlink-install'
+alias cbs='colcon build --symlink-install'
+alias cbp='colcon build --symlink-install --packages-select'
+alias cbu='colcon build --symlink-install --packages-up-to'
+alias ct='colcon test'
+alias ctp='colcon test --packages-select'
+alias ctr='colcon test-result'
+
+alias rt='ros2 topic list'
+alias re='ros2 topic echo'
+alias rn='ros2 node list'
+
+alias killgazebo='killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient'
+
+alias af='ament_flake8'
+alias ac='ament_cpplint'
+
+alias testpub='ros2 run demo_nodes_cpp talker'
+alias testsub='ros2 run demo_nodes_cpp listener'
+alias testpubimg='ros2 run image_tools cam2image'
+alias testsubimg='ros2 run image_tools showimage'
+
+# Set User Alias
+alias eb='gedit ~/.bashrc' 
+alias sb='source ~/.bashrc'
+" >> ~/.bashrc
+
+source ~/.bashrc
